d85eda00c601652c4f983e901805d95d1f9ea712
Lionel <lionelabatan@gmail.com>
2022-01-18 16:07:54 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TOUTOULIST';
+  list:any[]=[];
+  donelist:any[]=[];
+  addTask(item:string)
+  {
+    
+    this.list.push({id:this.list.length,name:item,completed:false})
+    console.warn(this.list);
+  }
+  removeTask(id:number)
+  {
+    console.warn(id)
+    this.list=this.list.filter(item=>item.id!==id)
+  }
+  completeTask(id:number,item:string)
+  {
+    console.warn(this.list[id])
+    this.list[id]
+    console.warn(this.list[id].completed)
+    this.list[id].completed= true
+    console.warn(this.list[id].completed)
+    if(this.list[id].completed == true){
+      this.donelist.push({id:this.list.length,name:item,completed:true})
+      this.list=this.list.filter(item=>item.id!==id)
+      console.warn(this.donelist)
+    
+    }
+  }
+  editTask(id:number){
+    this.list[id]
+  }
 }
